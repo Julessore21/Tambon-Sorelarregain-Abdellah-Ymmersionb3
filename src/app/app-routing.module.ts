b5d41ff3caac4cbbe4923986f97bccb816066dca
Julessore21 <Jules.sore13@gmail.com>
2023-10-05 11:34:02 +0200

    panier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
-<<<<<<< HEAD
 import { CartComponent } from './components/cart/cart.component';
-=======
 import { CheckoutComponent } from './checkout/checkout.component';
->>>>>>> 5d592b156e3bad898b72af64d5e44d85f8e8767b
 
 const routes: Routes = [
   {path: '', component: HomeComponent} ,
   {path: 'home', component: HomeComponent},
-<<<<<<< HEAD
   {path: 'polo/:id', component: DetailsComponent},
-  {path:'cart', component: CartComponent}  
-=======
+  {path:'cart', component: CartComponent},  
   {path: 'polo/:id', component: DetailsComponent},  
   {path: 'checkout', component: CheckoutComponent},  
 
->>>>>>> 5d592b156e3bad898b72af64d5e44d85f8e8767b
 ];
 
 @NgModule({
