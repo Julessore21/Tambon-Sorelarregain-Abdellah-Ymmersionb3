75fdf6b197608b30ff5c690f6ec1f986746697e8
LIGHTINGGG contact <jules.tambon@ynov.com>
2023-10-06 09:36:47 +0200

    filtre OK

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/core/services/data.service';
+import { polosService } from 'src/app/services/polos.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public title: string = "Shopping App";
 
   constructor(
-    public dataService: DataService
-  ){}
+    public dataService: DataService,
+    public polosService: polosService
+  ) {}
+
+  ngOnInit(): void {
+    this.polosService.getPolos();
+  }
+
+  triPrixCroissant(): void {
+    // Utilisez JavaScript pour trier les éléments par prix croissant
+    this.polosService.polos.sort((a, b) => a.price - b.price);
+    // Actualisez l'affichage des éléments triés
+    // Activez la classe "selected" pour "Prix croissant" et désactivez la classe pour "Prix décroissant"
+    document.getElementById('triPrixCroissant')?.classList.add('selected');
+    document.getElementById('triPrixDecroissant')?.classList.remove('selected');
+  }
+
+  triPrixDecroissant(): void {
+    // Utilisez JavaScript pour trier les éléments par prix décroissant
+    this.polosService.polos.sort((a, b) => b.price - a.price);
+    // Actualisez l'affichage des éléments triés
+    // Activez la classe "selected" pour "Prix décroissant" et désactivez la classe pour "Prix croissant"
+    document.getElementById('triPrixDecroissant')?.classList.add('selected');
+    document.getElementById('triPrixCroissant')?.classList.remove('selected');
+  }
 }
