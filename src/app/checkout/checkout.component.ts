5437c9c38bced9616017224b8d170f989cf5155a
LIGHTINGGG contact <jules.tambon@ynov.com>
2023-10-06 14:40:28 +0200

    OK mais juste delete panier qui manque

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent {
 
+export class CheckoutComponent implements OnInit {
+  public polos : any = [];
+  public grandTotal : number = 0;
+  public productsCart: any[] = [];
+
+  constructor(private cartService : CartService){}
+  ngOnInit(): void {
+    this.cartService.getProducts().subscribe((res: any)=>{
+      this.polos = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    })
+    this.listcart();
+    
+  }
+
+  listcart(){
+    const cart = localStorage.getItem("product");
+    console.log(cart)
+    if (cart != null){
+      this.productsCart.push(JSON.parse(cart));
+    }
+    console.log(this.productsCart)
+  }
+ 
+  removeItem(item : any){
+    this.cartService.removeCartItem(item);
+  }
+  emptycart(){
+    this.cartService.removeAllCart();
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  public totalItem: number = 0;
 
-  constructor(private router: Router) {}
+  public totalItem : number = 0;
+  constructor(private cartService : CartService) {}
 
-  allerVersMonCompte(): void {
-    // Redirigez l'utilisateur vers la page "Mon Compte"
-    this.router.navigate(['/mon-compte']);
+  ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.totalItem = res.length;
+    })
   }
 }
