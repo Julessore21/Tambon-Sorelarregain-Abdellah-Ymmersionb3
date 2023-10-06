98a0cd77ae0b10c20415b59f00c85d0464343b73
LIGHTINGGG contact <jules.tambon@ynov.com>
2023-10-06 08:54:08 +0200

    bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './components/footer/footer.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { CartComponent } from './components/cart/cart.component';
-import { ProductsComponent } from './components/products/products.component'
-import {MatButtonModule} from '@angular/material/button';
+import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
-import { CheckoutComponent } from './checkout/checkout.component'
+import { CheckoutComponent } from './checkout/checkout.component';
 
+// Importez ici le service PolosService
+import { PolosService } from './services/polos.service'; // Assurez-vous que le chemin d'importation est correct
 
 @NgModule({
   declarations: [
@@ -34,7 +34,10 @@ import { CheckoutComponent } from './checkout/checkout.component'
     MatIconModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    // Ajoutez le service PolosService ici
+    PolosService // Assurez-vous que le nom du service est correctement orthographié ici
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { DataService } from 'src/app/core/services/data.service';
-import { polosService } from 'src/app/services/polos.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,15 +9,6 @@ export class HomeComponent {
   public title: string = "Shopping App";
 
   constructor(
-    public polosService: polosService,
     public dataService: DataService
   ){}
-
-  ngOnInit() {
-    this.polosService.getPolos();
-  };
-
-  displayerBuypolo(){
-    console.log("Buy polo");
-  }
 }
