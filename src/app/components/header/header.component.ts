497247b01b8fc83ebc6dd4109d8d49eeb2b714ee
Julessore21 <Jules.sore13@gmail.com>
2023-10-06 08:49:59 +0200

    navbar / panier

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  public totalItem : number = 0;
+  constructor(private cartService : CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.totalItem = res.length;
+      
+    })
+  }
 }
--- src/app/services/polos.service.ts
+++ src/app/services/polos.service.ts
@@ -28,4 +28,8 @@ export class polosService {
       this.category = category;
     })
   }
+
+  addtocart(){
+    
+  }
 }
